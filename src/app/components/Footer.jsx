8f9dc2d1ef7dbafb4f81901b96976b38966e439e
haiffy420 <haifan.tribuwono@gmail.com>
2023-09-29 19:00:09 +0700

    master branch changed to main

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer border border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+      <div className="container p-12 flex justify-between">
+        <span className="text-4xl font-bold">Haifan.</span>
+        <p className="text-slate-400">All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
