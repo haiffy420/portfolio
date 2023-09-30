d5763dbb348252274abb3795f1db52964bce63ba
haiffy420 <haifan.tribuwono@gmail.com>
2023-09-30 09:11:17 +0700

    fix EmailSection

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -36,15 +36,20 @@ const EmailSection = () => {
   };
 
   return (
-    <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative" id="contact">
+    <section
+      className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
+      id="contact"
+    >
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-2">
-        <h5 className="text-xl font-bold text-white my-2">Let&rsquo;s Connect</h5>
+        <h5 className="text-xl font-bold text-white my-2">
+          Let&rsquo;s Connect
+        </h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
           {" "}
-          I&rsquo;m currently looking for new opportunities, my inbox is always open.
-          Whether you have a question or just want to say hi, I&rsquo;ll try my best
-          to get back to you!
+          I&rsquo;m currently looking for new opportunities, my inbox is always
+          open. Whether you have a question or just want to say hi, I&rsquo;ll
+          try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
           <Link href="https://github.com/haiffy420/">
@@ -57,9 +62,11 @@ const EmailSection = () => {
       </div>
       <div>
         {emailSubmitted ? (
-          <p className="text-green-500 text-sm mt-2">
-            Email sent successfully!
-          </p>
+          <div className="flex justify-center items-center">
+            <h5 className="text-xl font-bold text-white my-2">
+              Email sent successfully!
+            </h5>
+          </div>
         ) : (
           <form className="flex flex-col" onSubmit={handleSubmit}>
             <div className="mb-6">
