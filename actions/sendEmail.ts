ba50bdc2816a55bb72f03862b5e690df921dd8d2
haiffy420 <haifan.tribuwono@gmail.com>
2023-12-20 23:27:04 +0700

    Update: contact form

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -26,9 +26,9 @@ export const sendEmail = async (formData: FormData) => {
   let data;
   try {
     data = await resend.emails.send({
-      from: "Contact Form <onboarding@resend.dev>",
-      to: "haifan.tribuwono@gmail.com",
-      subject: "Message from contact form",
+      from: "Haifan <onboarding@resend.dev>",
+      to: ["haifan.tribuwono@gmail.com", senderEmail],
+      subject: "Haifan's Contact Form",
       reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
         message: message,
--- email/contact-form-email.tsx
+++ email/contact-form-email.tsx
@@ -24,17 +24,17 @@ export default function ContactFormEmail({
   return (
     <Html>
       <Head />
-      <Preview>New message from your portfolio site</Preview>
+      <Preview>Thank you for contacting me!</Preview>
       <Tailwind>
         <Body className="bg-gray-100 text-black">
           <Container>
             <Section className="bg-white borderBlack my-10 px-10 py-4 rounded-md">
               <Heading className="leading-tight">
-                You received the following message from the contact form
+                New Message Submitted:
               </Heading>
               <Text>{message}</Text>
-              <Hr />
-              <Text>The sender's email is: {senderEmail}</Text>
+              {/* <Hr />
+              <Text>The sender's email is: {senderEmail}</Text> */}
             </Section>
           </Container>
         </Body>
